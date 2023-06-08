db47047a32d2ee94aefc5cdeb27c0a985b0d8ecc
DaireFitz <131209841+DaireFitz@users.noreply.github.com>
2023-06-08 20:48:48 +0100

    Made page more responsive and added contact fourm

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,9 @@
 function toggleMobileMenu(menu){
     menu.classList.toggle('open');
+var contentWrapper = document.querySelector('.content-wrapper');
+  contentWrapper.classList.toggle('move-down');
+  var footer = document.querySelector('.footer');
+  footer.classList.toggle('move-down');
 }
+
+
