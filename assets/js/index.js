7e7c8e300fb1df079d0e4df3aa4c4b271b2e44c3
DaireFitz <131209841+DaireFitz@users.noreply.github.com>
2023-06-09 01:03:05 +0100

    Fixed some styling problems with h2 and formanting

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,7 @@
-function toggleMobileMenu(menu){
-    menu.classList.toggle('open');
-var contentWrapper = document.querySelector('.content-wrapper');
-  contentWrapper.classList.toggle('move-down');
-  var footer = document.querySelector('.footer');
-  footer.classList.toggle('move-down');
+function toggleMobileMenu(menu) {
+  menu.classList.toggle("open");
+  var contentWrapper = document.querySelector(".content-wrapper");
+  contentWrapper.classList.toggle("move-down");
+  var footer = document.querySelector(".footer");
+  footer.classList.toggle("move-down");
 }
-
